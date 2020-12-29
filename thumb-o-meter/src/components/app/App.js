7dc45516379288a00b11596e65a8f97e1b5c0c85
Your name herecod3rcarl <your_email@example.comcod3rcarl@gmail.com>
2020-12-29 14:43:15 +0000

    routing changes

--- thumb-o-meter/src/components/app/App.js
+++ thumb-o-meter/src/components/app/App.js
@@ -1,21 +1,32 @@
+import { useAuth0 } from "@auth0/auth0-react";
 import styles from "./app.module.css";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import Login from "../../pages/login/index";
-
+import LogoutButton from "../logout";
+import Admin from "../../pages/admin";
+import Menu from "../../pages/featureMenu";
+import Thumb from "../../pages/thumb-o-meter";
 function App() {
-  console.log("app component called");
-
+  const { isAuthenticated } = useAuth0();
   return (
     <Router>
       <div className={styles.app}>
         <Switch>
+          <Route path="/admin">
+            <Admin />
+          </Route>
+          <Route path="/menu">
+            <Menu />
+          </Route>
+          <Route path="/thumb">
+            <Thumb />
+          </Route>
           <Route path="/">
-            <Login />
+            {isAuthenticated ? <LogoutButton /> : <Login />}
           </Route>
         </Switch>
       </div>
     </Router>
   );
 }
-
 export default App;
--- thumb-o-meter/src/components/navBar/index.js
+++ thumb-o-meter/src/components/navBar/index.js
@@ -1,26 +1,24 @@
 import React from "react";
-import { BrowserRouter as Router, Link } from "react-router-dom";
+import { BrowserRouter as Link } from "react-router-dom";
 import { Icon, IconButton } from "@chakra-ui/react";
 import { FiHome } from "react-icons/fi";
 import LogoutButton from "../logout/index";
-
+import styles from "./navBar.module.css";
 function NavBar() {
   return (
-    <Router>
-      <nav>
-        <Link to="/featuremenu">
-          <IconButton
-            isRound="true"
-            colorScheme="teal"
-            aria-label="button"
-            icon={<Icon as={FiHome} />}
-          />
-        </Link>
-        <Link to="/logout">
-          <LogoutButton />
-        </Link>
-      </nav>
-    </Router>
+    <nav className={styles.nav}>
+      <Link to="/menu">
+        <IconButton
+          isRound="true"
+          colorScheme="teal"
+          aria-label="button"
+          icon={<Icon as={FiHome} />}
+        />
+      </Link>
+      <Link to="/logout">
+        <LogoutButton />
+      </Link>
+    </nav>
   );
 }
 export default NavBar;
