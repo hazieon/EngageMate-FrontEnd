a6d9028892f53da46b65685f0995747acb27d99f
Hazieon <handrew3721@gmail.com>
2020-12-29 14:14:39 +0000

    routing more

--- thumb-o-meter/src/components/app/App.js
+++ thumb-o-meter/src/components/app/App.js
@@ -1,6 +1,9 @@
 import styles from "./app.module.css";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import Login from "../../pages/login/index";
+import Admin from "../../pages/admin";
+import Menu from "../../pages/featureMenu";
+import Thumb from "../../pages/thumb-o-meter";
 
 function App() {
   console.log("app component called");
@@ -9,8 +12,17 @@ function App() {
     <Router>
       <div className={styles.app}>
         <Switch>
-          <Route path="/">
-            <Login />
+          <Route path="/admin">
+            <Admin />
+          </Route>
+          <Route path="/menu">
+            <Menu />
+          </Route>
+          <Route path="/thumb">
+            <Thumb />
+            <Route path="/">
+              <Login />
+            </Route>
           </Route>
         </Switch>
       </div>
