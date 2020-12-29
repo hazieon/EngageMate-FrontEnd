d98a59f3b1602b157d13da85b6ee2508dedb97df
= <beckyljones151192@gmail.com>
2020-12-29 23:43:09 +0000

    done some of the speaker view

--- thumb-o-meter/src/components/skView/index.js
+++ thumb-o-meter/src/components/skView/index.js
@@ -1,14 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
 import style from "./index.module.css";
+import { Select } from "@chakra-ui/react";
+import { Input } from "@chakra-ui/react";
+import { Button, ButtonGroup } from "@chakra-ui/react";
+import { MdUpdate } from "react-icons/md";
+import Thumb from "../thumb";
 
 function SkView() {
   // question
+  //maybe have dropdown (with preset questions) and input (for create questions)
   // Thumb
   // overall mood
-  // timer
+  // timer (start)
   // participants
 
-  return <div className={style.container}></div>;
+  const [value, setValue] = useState(0);
+
+  return (
+    <div className={style.container}>
+      {/* <h1>The Question Here</h1> */}
+      <Select placeholder="Select question">
+        <option value="option1">How are you feeling?</option>
+        <option value="option2">Did you understand that?</option>
+        <option value="option3">Are you comfortable with moving on?</option>
+      </Select>
+      <Input placeholder="Write your own question?" />
+
+      <Thumb value={value} />
+      <h3>Value: {value}%</h3>
+      <h4>Participants amount</h4>
+      <p>Timer Here</p>
+      {/* <Button
+        size="md"
+        height="35px"
+        width="150px"
+        border="2px"
+        borderColor="blue.500"
+      >
+        Start Timer
+      </Button> */}
+      <Button leftIcon={<MdUpdate />}>Start Timer</Button>
+    </div>
+  );
 }
 
 export default SkView;
