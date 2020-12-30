d3f78687092a8af73980602adc637b5cdb348b6a
= <=>
2020-12-30 10:13:02 +0000

    not on app

--- thumb-o-meter/src/components/app/App.js
+++ thumb-o-meter/src/components/app/App.js
@@ -6,7 +6,6 @@ import LogoutButton from "../logout";
 import Admin from "../../pages/admin";
 import Menu from "../../pages/featureMenu";
 import Thumb from "../../pages/thumb-o-meter";
-import Results from "../results/index";
 
 function App() {
   const { isAuthenticated } = useAuth0();
@@ -25,7 +24,6 @@ function App() {
           </Route>
           <Route path="/">
             {isAuthenticated ? <LogoutButton /> : <Login />}
-            <Results />
           </Route>
         </Switch>
       </div>
