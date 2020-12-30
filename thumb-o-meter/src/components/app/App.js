a944e913b058e1d54540fe6f26bd44466d2f6efd
Hazieon <handrew3721@gmail.com>
2020-12-30 12:21:29 +0000

    socket functions structure planning

--- thumb-o-meter/src/components/app/App.js
+++ thumb-o-meter/src/components/app/App.js
@@ -1,4 +1,5 @@
 import { useAuth0 } from "@auth0/auth0-react";
+import { useState, useEffect } from "react";
 import styles from "./app.module.css";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import Login from "../../pages/login/index";
@@ -6,9 +7,24 @@ import FeaturedMenu from "../../pages/featureMenu";
 import Admin from "../../pages/admin";
 import Menu from "../../pages/featureMenu";
 import Thumb from "../../pages/thumb-o-meter";
+import socketIOClient from "socket.io-client";
+const ENDPOINT = "http://localhost:3000";
 
 function App() {
   const { isAuthenticated } = useAuth0();
+  const [response, setResponse] = useState("");
+
+  useEffect(() => {
+    const socket = socketIOClient(ENDPOINT);
+    socket.emit("connection");
+
+    socket.on("FromAPI", (data) => {
+      setResponse(data);
+      console.log(response);
+    });
+    return () => socket.disconnect();
+  }, []);
+
   return (
     <Router>
       <div className={styles.app}>
