2e81261e4c9e81c05b4a02f34e93fdf31039f8f2
= <=>
2021-01-07 10:42:14 +0000

    basic hand component

--- thumb-o-meter/src/pages/raise-hand/index.js
+++ thumb-o-meter/src/pages/raise-hand/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import NavBar from "../../components/navBar";
 import CustomButton from "../../components/button";
 import { ArrowBackIcon } from "@chakra-ui/icons";
+import Hand from "../../components/hand";
 const RaiseHand = () => {
   return (
     <div>
