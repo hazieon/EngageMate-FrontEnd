407332963a76edb32bf0f0005a6b95d436800a1c
Hazieon <handrew3721@gmail.com>
2021-01-07 14:36:02 +0000

    speaker view functional CSS CHANGES NEEDED

--- thumb-o-meter/src/pages/raise-hand/index.js
+++ thumb-o-meter/src/pages/raise-hand/index.js
@@ -2,13 +2,35 @@ import React from "react";
 import NavBar from "../../components/navBar";
 import CustomButton from "../../components/button";
 import { ArrowBackIcon } from "@chakra-ui/icons";
+import useRoleContext from "../../context/roleContext";
 import SkHand from "../../components/skHand";
+import { Flex, Box, Center, useColorModeValue } from "@chakra-ui/react";
+import styles from "./index.module.css";
 const RaiseHand = () => {
+  const bg = useColorModeValue("white", "#110042");
+  const color = useColorModeValue("white", "white");
+  const result = useRoleContext();
+  const role = result[0];
+
   return (
     <div>
-      <NavBar />
-      <SkHand />
-      <CustomButton link="/" icon={<ArrowBackIcon />} text={"Back"} />
+      <Flex>
+        <Box className={styles.container} bg={bg} color={color}>
+          <main>
+            <NavBar />
+            <Center>
+              <h1 className={styles.heading}>Raise Hand</h1>
+            </Center>
+            <Center className={styles.backButton}>
+              <CustomButton link="/" icon={<ArrowBackIcon />} text={"Back"} />
+            </Center>
+            <Center>
+              {role !== "bootcamper" && <SkHand />}
+              {/* {role === "bootcamper" && <PtHand />} */}
+            </Center>
+          </main>
+        </Box>
+      </Flex>
     </div>
   );
 };
