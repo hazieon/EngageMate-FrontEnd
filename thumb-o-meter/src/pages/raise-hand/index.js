9bf19c4d760cc57b9eb841686845cdda3c9db1df
Hazieon <handrew3721@gmail.com>
2021-01-07 16:33:39 +0000

    conflicts sorted

--- thumb-o-meter/src/pages/raise-hand/index.js
+++ thumb-o-meter/src/pages/raise-hand/index.js
@@ -3,12 +3,10 @@ import NavBar from "../../components/navBar";
 import PtHand from "../../components/ptHand";
 import CustomButton from "../../components/button";
 import { ArrowBackIcon } from "@chakra-ui/icons";
-
 import useRoleContext from "../../context/roleContext";
 import SkHand from "../../components/skHand";
 import { Flex, Box, Center, useColorModeValue } from "@chakra-ui/react";
 import styles from "./index.module.css";
-
 
 const RaiseHand = () => {
   const bg = useColorModeValue("white", "#110042");
@@ -17,31 +15,23 @@ const RaiseHand = () => {
   const role = result[0];
 
   return (
-    <div>
-
-      <Flex>
-        <Box className={styles.container} bg={bg} color={color}>
-          <main>
-            <NavBar />
-            <Center>
-              <h1 className={styles.heading}>Raise Hand</h1>
-            </Center>
-            <Center className={styles.backButton}>
-              <CustomButton link="/" icon={<ArrowBackIcon />} text={"Back"} />
-            </Center>
-            <Center>
-              {role !== "bootcamper" && <SkHand />}
-              {/* {role === "bootcamper" && <PtHand />} */}
-            </Center>
-          </main>
-        </Box>
-      </Flex>
-
-      <NavBar />
-      <PtHand />
-      <CustomButton link="/" icon={<ArrowBackIcon />} text={"Back"} />
-
-    </div>
+    <Flex>
+      <Box className={styles.container} bg={bg} color={color}>
+        <main>
+          <NavBar />
+          <Center>
+            <h1 className={styles.heading}>Raise Hand</h1>
+          </Center>
+          <Center className={styles.backButton}>
+            <CustomButton link="/" icon={<ArrowBackIcon />} text={"Back"} />
+          </Center>
+          <Center>
+            {role !== "bootcamper" && <SkHand />}
+            {role === "bootcamper" && <PtHand />}
+          </Center>
+        </main>
+      </Box>
+    </Flex>
   );
 };
 
