05bdb5ee7577c871d4adba2807eaaf366623038e
Your name herecod3rcarl <your_email@example.comcod3rcarl@gmail.com>
2021-01-08 08:57:56 +0000

    changed back config file

--- thumb-o-meter/src/config.js
+++ thumb-o-meter/src/config.js
@@ -1,2 +1,2 @@
-// export const config = { url: process.env.REACT_APP_url };
-export const config = { url: "http://localhost:5000" };
+export const config = { url: process.env.REACT_APP_url };
+// export const config = { url: "http://localhost:5000" };
