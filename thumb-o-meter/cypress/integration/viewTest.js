e25ec82ca5340526e252585bb4c896a0458d7136
= <beckyljones151192@gmail.com>
2021-01-08 11:46:12 +0000

    testing git issues ignore this message

--- thumb-o-meter/cypress/integration/viewTest.js
+++ thumb-o-meter/cypress/integration/viewTest.js
@@ -1,6 +1,8 @@
 import ParticipantViewTest from "./participantView/participantViewCompleteTest";
 import SpeakerViewTest from "./speakerView/speakerViewCompleteTest";
 
+//need to look at login and logout
+
 function viewTest() {
   ParticipantViewTest();
   SpeakerViewTest();
