10c4e656f333fd19d94beb2770c77072a2019636
Hazieon <handrew3721@gmail.com>
2021-01-12 16:49:01 +0000

    got started with participant poll conditional rendering and results bars

--- thumb-o-meter/src/components/ptPoll/index.js
+++ thumb-o-meter/src/components/ptPoll/index.js
@@ -1,16 +1,44 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import style from "./index.module.css";
-import { Select, Button, Radio } from "@chakra-ui/react";
+import {
+  Select,
+  Button,
+  Radio,
+  SliderTrack,
+  SliderFilledTrack,
+  Progress,
+} from "@chakra-ui/react";
 
 function PtPoll({ data }) {
   const [myColor, setMyColor] = useState("#2C276B");
-  const [session, setSession] = useState(true);
+  const [session, setSession] = useState(false);
+  const [results, setResults] = useState(true);
 
+  //hardcoded example resultsData, needs to take in from real data
+  let resultsData = {
+    option1: 20,
+    option2: 30,
+    option3: 40,
+    option4: 10,
+  };
+
+  function sessionResults() {
+    //sets the results state to be the results data as sent in
+    if (resultsData) {
+      setResults(resultsData);
+      console.log(resultsData);
+    }
+  }
+  useEffect(() => {
+    sessionResults();
+  }, [results]);
   //results state - if results state is true, sets session state to FALSE
   //if results is true AND session is false- displays progress bars with results data
-  //percentage results of each option
+  //percentage results of each optiong
 
   function sessionActive() {
+    //if question and option data is set, set session activity to 'true'
+    //session 'true' conditionally renders
     if (data) {
       setSession(true);
     }
@@ -32,8 +60,42 @@ function PtPoll({ data }) {
           </section>
         </div>
       ) : (
-        <h1>waiting for poll</h1>
+        <div>
+          <h2>Waiting for Poll...</h2>
+        </div>
       )}
+      {results ? (
+        <div>
+          <h1>Poll Results :</h1>
+          <Progress
+            className={results.option1 ? style.results : style.resultsNone}
+            colorScheme={"green"}
+            height="5vh"
+            value={results.option1}
+          />
+          <Progress
+            className={results.option2 ? style.results : style.resultsNone}
+            colorScheme={"green"}
+            height="5vh"
+            value={results.option2}
+          />
+          <Progress
+            className={results.option3 ? style.results : style.resultsNone}
+            colorScheme={"green"}
+            height="5vh"
+            value={results.option3}
+          />
+          <Progress
+            className={results.option4 ? style.results : style.resultsNone}
+            colorScheme={"green"}
+            height="5vh"
+            value={results.option4}
+          />
+        </div>
+      ) : (
+        <h2>Results pending</h2>
+      )}
+
       <Button type="submit">Submit ➡</Button>
     </div>
   );
