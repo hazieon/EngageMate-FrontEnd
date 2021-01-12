a3259e52b2c5acdd5249cb9a6924699189548936
= <beckyljones151192@gmail.com>
2021-01-12 13:08:27 +0000

    created a still working folder for cypress test and will be moving functions into it that still work with the current dev

---
+++ thumb-o-meter/cypress/integration/stillWorks/stillWorkingTest.js
@@ -0,0 +1,7 @@
+import openingScreen from "./openingScreen/openingScreen";
+import logIn from "./logIn/logIn";
+import logOut from "./logOut/logOut";
+
+openingScreen();
+logIn("speakerview@gmail.com", "(callbackCats)");
+logOut();
