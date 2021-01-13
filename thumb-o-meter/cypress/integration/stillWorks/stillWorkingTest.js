1ee2743a355606361b078d94a6a979b2798f9b3a
= <beckyljones151192@gmail.com>
2021-01-13 14:38:44 +0000

    added a test that changes screen from dark to light mode

--- thumb-o-meter/cypress/integration/stillWorks/stillWorkingTest.js
+++ thumb-o-meter/cypress/integration/stillWorks/stillWorkingTest.js
@@ -7,12 +7,13 @@ import checkingForQuizFeature from "./featureMenu/checkingQuiz/checkQuiz";
 import speakerThumbSession from "./speaker/speakerThumb/speakerThumb";
 import backButton from "./backButton/backButton";
 import logOut from "./logOut/logOut";
-
+import changingBetweenDarkAndLightMode from "./darkLightMode";
 openingScreen();
 logIn("speakerview@gmail.com", "(callbackCats)");
-checkingForThumbOMeterFeature();
-speakerThumbSession();
-backButton();
+changingBetweenDarkAndLightMode();
+//checkingForThumbOMeterFeature();
+// speakerThumbSession();
+// backButton();
 // checkingForHandFeature();
 // checkingForQuizFeature();
 //logOut();
