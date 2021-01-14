83280a264c2b9d3cfd27676901d8d14d5b2228a7
= <=>
2021-01-14 17:22:31 +0000

    all logo variations added, tab image changed

--- thumb-o-meter/src/pages/login/index.js
+++ thumb-o-meter/src/pages/login/index.js
@@ -1,12 +1,23 @@
 import React from "react";
 import LoginButton from "../../components/loginButton";
 import styles from "./index.module.css";
-import { Center } from "@chakra-ui/react";
+import { Center, HStack } from "@chakra-ui/react";
+import Subheading from "../../components/subheading";
 
 function Login() {
   return (
     <Center className={styles.container}>
-      <LoginButton />
+      <HStack>
+        <img
+          className={styles.img}
+          src="/EngageMateImageTextDark.png"
+          alt="EngageMate Logo"
+        />
+        <Subheading
+          text={"Measures and Tracks Engagement In Real Time"}
+        ></Subheading>
+        <LoginButton />
+      </HStack>
     </Center>
   );
 }
