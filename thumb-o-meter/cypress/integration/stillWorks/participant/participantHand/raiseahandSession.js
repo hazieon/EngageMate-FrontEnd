8ee05d98a987ce0fadf64df79cd72d04ed4dd1a5
= <beckyljones151192@gmail.com>
2021-01-17 20:33:21 +0000

    raise a hand test done now

--- thumb-o-meter/cypress/integration/stillWorks/participant/participantHand/raiseahandSession.js
+++ thumb-o-meter/cypress/integration/stillWorks/participant/participantHand/raiseahandSession.js
@@ -1,17 +1,17 @@
-function raiseHandSession() {
+function participantRaiseHandSession() {
   describe("Participant raising hand session", () => {
     it("Find input field and type topic into it, find image and click to raise the hand", () => {
       cy.get("input")
         .should("have.class", "chakra-input css-1m7lgs4")
-        .type("Testing?");
+        .type("Test?");
     });
   });
 
   describe("Participant raising hand session", () => {
-    it("Find input field and type topic into it, find image and click to raise the hand", () => {
+    it("Find image and click to raise the hand", () => {
       cy.get("img").should("have.class", "hand_hand__2-yiY").click();
     });
   });
 }
 
-export default raiseHandSession;
+export default participantRaiseHandSession;
