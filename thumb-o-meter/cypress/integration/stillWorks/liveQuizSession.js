0d24b8c9d843035ae380b4d5a31ceec99cff7a9c
= <beckyljones151192@gmail.com>
2021-01-24 17:02:40 +0000

    changed live quiz test slightly

--- thumb-o-meter/cypress/integration/stillWorks/liveQuizSession.js
+++ thumb-o-meter/cypress/integration/stillWorks/liveQuizSession.js
@@ -1,34 +1,3 @@
-//opening screen
-//log in speakerview
-//dark mode
-//check feature menu
-//click quiz
-//check quiz page
-//select custom question
-//type in input field
-//click to add option
-//type in option 1
-//click add option
-//type in option 2
-//click add option
-//type in option 3
-//click add to option
-//click remove option
-//select correct answer
-//click submit
-//log out
-//log in participant
-//dark mode
-//check feature menu
-//click live quiz
-//check live quiz page
-//find question confirm has correct text
-//find option(s) confirm has correct text
-//click select on an option
-//click submit
-//find result check 1
-//log out
-
 import openingScreen from "./openingScreen/openingScreen";
 import logIn from "./logIn/logIn";
 import darkMode from "./darkMode/darkMode";
--- thumb-o-meter/cypress/integration/stillWorks/speaker/speakerQuiz/liveQuizSpeakerSessionAfter.js
+++
@@ -1,9 +0,0 @@
-import speakerLiveQuizSession from "./liveQuizSpeakerSession";
-
-function speakerLiveQuizSessionAfter() {
-  describe("Speaker session after getting participant answer", () => {
-    it("Check for participant answer", () => {});
-  });
-}
-
-export default speakerLiveQuizSession;
